fd8dbad2762f5bc215e8ea3c8ff0d26346f06413
Szijártó Csaba István <csaba.szijarto03@gmail.com>
2026-03-02 17:17:30 +0100

    file structure reorganization

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
